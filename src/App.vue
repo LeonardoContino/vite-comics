b7bd3ab288dc41b966e072658d8bc270b79315f9
LeonardoContino <leoconti2000@hotmail.it>
2023-01-20 15:49:51 +0100

    inizio header

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,21 @@
 </script>
 
 <template>
- 
+ <div class="container border">
+
+ </div>
   
 </template>
 
 <style>
 body {
   font-family: "Open Sans", sans-serif;
+}
+.container{
+    width: 1200px;
+    margin: auto;
+}
+.border{
+    border: 2px dashed black;
 }
 </style>
